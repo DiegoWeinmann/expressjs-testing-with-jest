e42a35dd97e9d4d20bf0ad1e1c770d79bc06d9b8
Diego Weinmann <diego.weinmann@hotmail.com>
2020-08-19 23:25:12 -0300

    feat: getTodoById not found error handler

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -52,7 +52,14 @@ describe('TodoController.getTodoById', () => {
     const rejectedPromise = Promise.reject(errorMessage);
     TodoModel.findById.mockReturnValue(rejectedPromise);
     await TodoController.getTodoById(req, res, next);
-    expect(next).toBeCalledWith(errorMessage);
+    expect(next).toHaveBeenCalledWith(errorMessage);
+  });
+
+  it('should return 404 when todo does not exist.', async () => {
+    TodoModel.findById.mockReturnValue(null);
+    await TodoController.getTodoById(req, res, next);
+    expect(res.statusCode).toBe(404);
+    expect(res._isEndCalled()).toBeTruthy();
   });
 });
 
@@ -81,7 +88,7 @@ describe('TodoController.getTodos', () => {
     const rejectedPromise = Promise.reject(errorMessage);
     TodoModel.find.mockReturnValue(rejectedPromise);
     await TodoController.getTodos(req, res, next);
-    expect(next).toBeCalledWith(errorMessage);
+    expect(next).toHaveBeenCalledWith(errorMessage);
   });
 });
 
